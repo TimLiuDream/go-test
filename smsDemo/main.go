b150a8485ab7be717fe432926de25c8fed4aaabb
TimLiu <timliudream@163.com>
2018-07-16 19:02:58 +0800

    清除appkey

--- smsDemo/main.go
+++ smsDemo/main.go
@@ -9,8 +9,8 @@ import (
 
 var (
 	gatewayUrl      = "http://dysmsapi.aliyuncs.com/"
-	accessKeyId     = "LTAIWxWZXPLYT5px"
-	accessKeySecret = "tO11eWbHMR1zlzc3M7OvbEXqs74jPf"
+	accessKeyId     = "LTAIWxW"
+	accessKeySecret = "tO11eWbHMR1zlzc3M"
 	phoneNumbers    = "15088131357"
 	signName        = "刘佛添"
 	templateCode    = "SMS_139810284"
