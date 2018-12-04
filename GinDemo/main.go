7c4d961fc1244e41d899602982420978a816f2c1
TimLiu <417806161@qq.com>
2018-12-04 22:22:22 +0800

    gindemo集成swagger

--- GinDemo/main.go
+++ GinDemo/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"github.com/jinzhu/gorm"
-	"fmt"
 	"GolangTraining/GinDemo/Model"
+	"fmt"
+	"net/http"
+
+	_ "GolangTraining/GinDemo/docs"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 //字段名一定要大写，不然不能导出，那就不能被请求体的json反序列化
@@ -17,6 +22,14 @@ type TestJson struct {
 var db *gorm.DB
 var err error
 
+// @title Gin Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
 func main() {
 	db, err = gorm.Open("mysql", "root:lft8306488@tcp(localhost:3306)/todo_list_dev?charset=utf8&parseTime=True&loc=Local")
 	defer db.Close()
@@ -34,9 +47,16 @@ func main() {
 		})
 	})
 	r.POST("/change", changePost)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run()
 }
 
+// @Summary 测试post
+// @Accept  json
+// @Produce  json
+// @Param Para body TestJson true "para"
+// @Success 200 {string} json "{"message": 1991}"
+// @Router /change [post]
 func changePost(context *gin.Context) {
 	var test TestJson
 	if err := context.BindJSON(&test); err != nil {
