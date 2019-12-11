35c7c5deac4a9ca6ac23140a9a61b7fc5960b5b5
liufotian <timliudream@163.com>
2019-12-11 10:54:53 +0800

    update:goroutine

--- goroutine/main.go
+++ goroutine/main.go
@@ -5,14 +5,60 @@ import (
 	"runtime"
 )
 
-func say(s string) {
+func main() {
+	go func1("world")
+	func1("hello")
+}
+
+func func1(s string) {
 	for i := 0; i < 5; i++ {
 		runtime.Gosched()
 		fmt.Println(s)
 	}
 }
 
-func main() {
-	go say("world")
-	say("hello")
+func func2() {
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
+}
+
+func sum(a []int, c chan int) {
+	total := 0
+	for _, v := range a {
+		total += v
+	}
+	c <- total
+}
+
+func func3() {
+	c := make(chan int, 2)
+
+	c <- 1
+	c <- 2
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(c)
+}
+
+func func4() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
 }
