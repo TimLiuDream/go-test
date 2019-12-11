da45bbe49704950f7ce5ebaf0e8f48f552d28f3e
liufotian <timliudream@163.com>
2019-12-11 10:51:22 +0800

    update:channel

--- channel/main.go
+++ channel/main.go
@@ -24,6 +24,17 @@ func func2() {
 	fmt.Println("func2 init chan an send value")
 }
 
+func func3() {
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
+		}
+	}
+}
+
 func main() {
 	go func1()
 	time.Sleep(5 * time.Second)
