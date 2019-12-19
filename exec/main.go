9917fac4ccdd303bd5b9850724cf83349bf2fbee
liufotian <timliudream@163.com>
2019-12-19 19:37:27 +0800

    update:exec

--- exec/main.go
+++ exec/main.go
@@ -1,59 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 	"time"
 )
 
 const (
-	originPath       = "/Users/tim/Documents/KAk8aGVf_8DtmcCcR_1575012771030810.html"
+	originPath       = "/Users/tim/Downloads/普通测试文件.html"
 	targetPath       = "/Users/tim/Downloads/test.docx"
 	referenceDocPath = "/Users/tim/Downloads/custom-reference.docx"
 )
 
+const (
+	TimeOutSecond = 55
+)
+
 func main() {
 	cmd := exec.Command("pandoc", "-f", "HTML", originPath, "-o", targetPath, "--reference-doc", referenceDocPath)
-	go listenPandoc(cmd)
+	go listenPandoc(cmd, "234567")
 	err := cmd.Run()
 	if err != nil {
 		if err.Error() == "signal: killed" {
 			log.Println("over time!")
 		}
+		fmt.Println("here")
 		log.Fatalln(err.Error())
 	}
 	log.Println("complete!")
 }
 
-func listenPandoc(cmd *exec.Cmd) {
-	for cmd.Process == nil {
-		continue
-	}
-	i := 1
-	for cmd.Process != nil {
-		if i == 55 {
-			log.Println("kill pandoc?")
-			err := cmd.Process.Kill()
-			if err != nil {
-				log.Println(err.Error())
-			}
-			return
-		}
-		time.Sleep(1 * time.Second)
-		i++
-		if cmd.ProcessState == nil {
-			continue
-		}
-		if cmd.ProcessState.String() == "signal: killed" {
-			return
-		}
-		log.Println("Exited:", cmd.ProcessState.Exited())
-		if cmd.ProcessState.Exited() {
-			return
-		}
-		log.Println("cmd.ProcessState.Success():", cmd.ProcessState.Success())
-		if cmd.ProcessState.Success() {
-			return
-		}
+func listenPandoc(cmd *exec.Cmd, pageUUID string) {
+	<-time.After(time.Second * TimeOutSecond)
+	if cmd.Process == nil {
+		return
+	} else {
+		log.Printf("kill pandoc, pageUUID: %s\n", pageUUID)
+		_ = cmd.Process.Kill()
+		return
 	}
 }
