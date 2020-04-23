a77dfe377ffbc6c58aac6b1234fbd22bbc5f00a6
liufotian <timliudream@163.com>
2020-04-23 20:56:57 +0800

    update:test

---
+++ test/testing_test.go
@@ -0,0 +1,27 @@
+package test
+
+import "testing"
+
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+func TestFib(t *testing.T) {
+	var (
+		in       = 7
+		expected = 13
+	)
+	actual := Fib(in)
+	if actual != expected {
+		t.Errorf("Fib(%d) = %d; expected %d", in, actual, expected)
+	}
+}
+
+func BenchmarkFib(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fib(7)
+	}
+}
