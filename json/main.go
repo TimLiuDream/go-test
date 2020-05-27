d45f47083baed0930fc2be9d4ee93bc31ca5b680
liufotian <timliudream@163.com>
2020-05-27 10:42:03 +0800

    update:json

--- json/main.go
+++ json/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
 	// "github.com/chanxuehong/util/json"
 )
@@ -30,7 +31,8 @@ type firstCase struct {
 
 func main() {
 	// func1()
-	func3()
+	// func3()
+	func4()
 }
 
 func func1() {
@@ -109,4 +111,244 @@ func func3() {
 		}
 	}
 	fmt.Println(test.Changelogs)
+}
+
+func func4() {
+	str := `
+	{
+		"activity_chart": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"activity_draft": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"card": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"component": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"component_template": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"component_view": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"component_view_user": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"dashboard": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"deliverable": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"department": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"field": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"field_option": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"filter": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"gantt_chart": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"gantt_data": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"issue_type": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"manhour_report": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"milestone": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"organization": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"pipeline": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"plugin": {
+			"uuid_index": 0,
+			"name_index": 1
+		},
+		"ppm_task": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"product": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"product_component": {
+			"uuid_index": 0,
+			"name_index": 11
+		},
+		"product_component_view": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"product_module": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"program": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"program_role": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"project": {
+			"uuid_index": 0,
+			"name_index": 1
+		},
+		"project_field": {
+			"uuid_index": 0,
+			"name_index": 6
+		},
+		"project_filter": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"project_report": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"project_sprint_field": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"project_sprint_field_option": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"project_sprint_status": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"resource": {
+			"uuid_index": 0,
+			"name_index": 9
+		},
+		"role": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"sprint": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"task": {
+			"uuid_index": 0,
+			"name_index": 13
+		},
+		"task_gantt_chart": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"task_gantt_data": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"task_link_type": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"task_status": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"team": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"team_report": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"testcase_case": {
+			"uuid_index": 0,
+			"name_index": 6
+		},
+		"testcase_case_step": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"testcase_field": {
+			"uuid_index": 0,
+			"name_index": 7
+		},
+		"testcase_field_config": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"testcase_library": {
+			"uuid_index": 0,
+			"name_index": 2
+		},
+		"testcase_module": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"testcase_plan": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"transition": {
+			"uuid_index": 0,
+			"name_index": 6
+		},
+		"user": {
+			"uuid_index": 0,
+			"name_index": 5
+		},
+		"user_filter_view": {
+			"uuid_index": 0,
+			"name_index": 4
+		},
+		"user_group": {
+			"uuid_index": 0,
+			"name_index": 3
+		},
+		"version": {
+			"uuid_index": 0,
+			"name_index": 3
+		}
+	}
+	`
+	m := make(map[string]map[string]int)
+	err := json.Unmarshal([]byte(str), &m)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
