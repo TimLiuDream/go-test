7d4b696318c86e508905821720ae3d6f65782834
liufotian <timliudream@163.com>
2023-03-07 11:47:16 +0800

    update:float

--- float/main.go
+++ float/main.go
@@ -6,6 +6,17 @@ import (
 )
 
 func main() {
+	func1()
+}
+
+func func1() {
+	o := 5500000
+	n := 10000000
+	v := o / n
+	fmt.Println(v)
+}
+
+func func2() {
 	fixedValue := ""
 	value := "902000"
 	rawFloat, err := strconv.ParseFloat(value, 64)
