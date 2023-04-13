79e8e2f921330c0d7f773a8919b80afd1e742d99
liufotian <timliudream@163.com>
2023-04-13 18:06:39 +0800

    update:kafka

--- kafka/consumer/main.go
+++ kafka/consumer/main.go
@@ -1,60 +1,60 @@
 package main
 
-// kafka consumer by kafka-go
-
 import (
 	"context"
 	"fmt"
-	kafka "github.com/segmentio/kafka-go"
+	"github.com/Shopify/sarama"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
-func main() {
-	config := kafka.ReaderConfig{
-		Brokers: []string{"localhost:9092"},
-		GroupID: "my-group",
-		Topic:   "test",
-		// MinBytes: 10e3, // 10KB
-		// MaxBytes: 10e6, // 10MB
-		// MaxWait:  1 * time.Second,
-	}
+// kafka consumer by kafka-go
 
-	r := kafka.NewReader(config)
-	defer r.Close()
-	offsetReq := kafka.TimeOffsetOf(0, time.Now().Add(-1*time.Hour))
-	r.SetOffsetAt()
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	go func() {
-		sig := <-signals
-		fmt.Printf("Caught signal %s: terminating\n", sig)
-		cancel()
-	}()
-
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("terminating: context cancelled")
-			return
-		default:
-			m, err := r.ReadMessage(ctx)
-			if err != nil {
-				log.Println(err)
-				break
-			}
-			log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
-		}
-	}
-}
+//
+//func main() {
+//	config := kafka.ReaderConfig{
+//		Brokers: []string{"localhost:9092"},
+//		GroupID: "my-group",
+//		Topic:   "test",
+//		// MinBytes: 10e3, // 10KB
+//		// MaxBytes: 10e6, // 10MB
+//		// MaxWait:  1 * time.Second,
+//	}
+//
+//	r := kafka.NewReader(config)
+//	defer r.Close()
+//	offsetReq := kafka.TimeOffsetOf(0, time.Now().Add(-1*time.Hour))
+//	r.SetOffsetAt()
+//
+//	signals := make(chan os.Signal, 1)
+//	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+//
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+//
+//	go func() {
+//		sig := <-signals
+//		fmt.Printf("Caught signal %s: terminating\n", sig)
+//		cancel()
+//	}()
+//
+//	for {
+//		select {
+//		case <-ctx.Done():
+//			fmt.Println("terminating: context cancelled")
+//			return
+//		default:
+//			m, err := r.ReadMessage(ctx)
+//			if err != nil {
+//				log.Println(err)
+//				break
+//			}
+//			log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+//		}
+//	}
+//}
 
 // Compare this snippet from kafka/consumer/main.go:
 // package main
@@ -73,69 +73,69 @@ func main() {
 // )
 //
 
-//func main() {
-//	config := sarama.NewConfig()
-//	config.Consumer.Return.Errors = true
-//	config.Consumer.Offsets.AutoCommit.Enable = false
-//	config.Version = sarama.V2_0_0_0
-//
-//	client, err := sarama.NewClient([]string{"localhost:9092"}, config)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//	defer client.Close()
-//
-//	consumer, err := sarama.NewConsumerFromClient(client)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//	defer consumer.Close()
-//
-//	signals := make(chan os.Signal, 1)
-//	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-//
-//	ctx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	go func() {
-//		sig := <-signals
-//		fmt.Printf("Caught signal %s: terminating\n", sig)
-//		cancel()
-//	}()
-//
-//	topic := "test"
-//	partitionList, err := consumer.Partitions(topic)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//
-//	for partition := range partitionList {
-//		// how to use GetOffset
-//		//offset, err := client.GetOffset(topic, int32(partition), sarama.OffsetNewest)
-//		//if err != nil {
-//		//	panic(err)
-//		//}
-//		//log.Printf("offset: %d", offset)
-//		pc, err := consumer.ConsumePartition(topic, int32(partition), 20)
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//		defer pc.AsyncClose()
-//
-//		go func(sarama.PartitionConsumer) {
-//			for {
-//				select {
-//				case <-ctx.Done():
-//					fmt.Println("terminating: context cancelled")
-//					return
-//				case msg := <-pc.Messages():
-//					log.Printf("Partition:%d Offset:%d Key:%s Value:%s", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
-//				case err := <-pc.Errors():
-//					log.Printf("PartitionConsumer error: %s", err)
-//				}
-//			}
-//		}(pc)
-//	}
-//
-//	<-ctx.Done()
-//}
+func main() {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.AutoCommit.Enable = false
+	config.Version = sarama.V2_0_0_0
+
+	client, err := sarama.NewClient([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	consumer, err := sarama.NewConsumerFromClient(client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer consumer.Close()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sig := <-signals
+		fmt.Printf("Caught signal %s: terminating\n", sig)
+		cancel()
+	}()
+
+	topic := "test"
+	partitionList, err := consumer.Partitions(topic)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for partition := range partitionList {
+		// how to use GetOffset
+		//offset, err := client.GetOffset(topic, int32(partition), sarama.OffsetNewest)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//log.Printf("offset: %d", offset)
+		pc, err := consumer.ConsumePartition(topic, int32(partition), 20)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer pc.AsyncClose()
+
+		go func(sarama.PartitionConsumer) {
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("terminating: context cancelled")
+					return
+				case msg := <-pc.Messages():
+					log.Printf("Partition:%d Offset:%d Key:%s Value:%s", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+				case err := <-pc.Errors():
+					log.Printf("PartitionConsumer error: %s", err)
+				}
+			}
+		}(pc)
+	}
+
+	<-ctx.Done()
+}
