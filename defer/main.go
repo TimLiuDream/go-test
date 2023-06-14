8f05be54f3c892a9ed36fd854547b60414eba93e
tim <liufotian@gmail.com>
2023-06-14 17:20:40 +0800

    update:defer

--- defer/main.go
+++ defer/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"runtime/debug"
 )
 
 func main() {
-	//testFunc1()
-	func2()
+	func6()
 }
 
 func goFun() {
@@ -55,4 +55,63 @@ func func2() {
 	tt := &temp{}
 	defer tt.Add(1).Add(2)
 	tt.Add(3)
+}
+
+// 当异常被抛出时，程序的控制流会立即跳转到最近的恢复点，也就是包含当前 defer 语句的代码块的末尾。
+func defer_call() {
+	defer func() { fmt.Println("打印前") }()
+	defer func() { fmt.Println("打印中") }()
+	defer func() { fmt.Println("打印后") }()
+
+	panic("触发异常")
+}
+
+func func3() (r int) {
+	i := 1
+	defer func() {
+		i = i + 1
+	}()
+	return i
+}
+
+func func4() (r int) {
+	defer func(rq int) {
+		r = r + 2
+	}(r)
+	return 2
+}
+
+func func5() (r int) {
+	defer func(r *int) {
+		*r = *r + 2
+	}(&r)
+	return 2
+}
+
+func func6() {
+	e1()
+	e2()
+	e3()
+}
+
+func e1() {
+	var err error
+	defer fmt.Println(err)
+	err = errors.New("e1 defer err")
+}
+
+func e2() {
+	var err error
+	defer func() {
+		fmt.Println(err)
+	}()
+	err = errors.New("e2 defer err")
+}
+
+func e3() {
+	var err error
+	defer func(err error) {
+		fmt.Println(err)
+	}(err)
+	err = errors.New("e3 defer err")
 }
